c3f16cc53de7e0118f027a932923c2adfb3ac39b
TDC28 <a06x03g07t01@protonmail.com>
2024-06-12 23:36:41 -0300

    Added lazygit plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,25 @@
 local overrides = require("configs.overrides")
 return {
 	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		-- setting the keybinding for LazyGit with 'keys' is recommended in
+		-- order to load the plugin when the command is run for the first time
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+		},
+	},
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
