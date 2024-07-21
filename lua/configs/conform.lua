b4f0694f807aa9871abd82945351387c174203e9
TDC28 <a06x03g07t01@protonmail.com>
2024-07-21 13:01:14 -0300

    Updates to lsp and conform

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,16 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    bash = { "shfmt" },
-    tex = { "latexindent" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		bash = { "shfmt" },
+		tex = { "latexindent" },
+		cpp = { "clang-format" },
+	},
 
-  -- format_on_save = {
-  --   timeout_ms = 10000,
-  --   lsp_fallback = true,
-  -- },
+	-- format_on_save = {
+	--   timeout_ms = 10000,
+	--   lsp_fallback = true,
+	-- },
 }
 
 require("conform").setup(options)
